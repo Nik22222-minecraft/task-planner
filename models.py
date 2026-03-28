cdbe6be3bd07209d2104fdb476cb19430b10afe3
Nik22222-minecraft <nikitaalisejko879@gmail.com>
2026-03-28 15:10:13 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# Инициализируем объект базы данных (без привязки к приложению)
 db = SQLAlchemy()
 
 class User(db.Model):
     """Модель пользователя системы."""
-    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -13,13 +11,14 @@ class User(db.Model):
     telegram_id = db.Column(db.String(100), nullable=True)
     is_public = db.Column(db.Boolean, default=False)
     
-    # Связь с задачами (Один ко многим)
+    # НОВОЕ ПОЛЕ: Часовой пояс пользователя (по умолчанию UTC+3, Москва)
+    timezone = db.Column(db.Integer, default=3)
+    
     tasks = db.relationship('Task', backref='owner', lazy=True)
 
 
 class Task(db.Model):
     """Модель задачи в планировщике."""
-    
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     category = db.Column(db.String(50), default='Общее')
@@ -29,6 +28,4 @@ class Task(db.Model):
     description = db.Column(db.Text, default="")
     date = db.Column(db.String(10), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    
-    # Внешний ключ, указывающий на владельца задачи
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
