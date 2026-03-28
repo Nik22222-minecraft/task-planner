6e68218edfe76c81f118419627411886b8f6c246
Nik22222-minecraft <nikitaalisejko879@gmail.com>
2026-03-28 15:53:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     # Секретный ключ (для GitHub можно оставить любой тестовый)
     SECRET_KEY = 'super-secret-key-change-me'
     
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    # Путь к базе данных (с тайм-аутом от блокировок)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db') + '?timeout=30'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # ВАЖНО: Удали свой реальный токен перед коммитом!
